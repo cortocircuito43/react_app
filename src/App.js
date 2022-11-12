19fc3a53926931988b98951daf02a58e880005d4
cortocircuito43 <irampez@gmail.com>
2022-11-11 23:51:31 -0500

    React components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react'
+import './normalize.css'
 import './App.css';
+import {CardPreview } from './components/cardPreview'
 
 function App() {
   return (
     <div >
       <header>
-        <h1>Study Deck App</h1>
+        <h1>Study<span className="titleHighlight">Deck</span>App</h1>
         <h2>Retention through Repetition</h2>
       </header>
       <main>
         <h3>FlaschCards</h3>
-        <div>
-          <div>
-            <h4>Term goes Here</h4>
-            <div>
-              <button type="button">show Back</button>
-              <button type="button">Edit</button>
-              <button type="button">Delete</button>
-
-            </div>
-          </div>
+        <div className='gridContainer'>
+         <CardPreview term="What does a duck say"/>
+         <CardPreview term="What does a perro say"/>
+         <CardPreview term="What does a gato say"/>
+         
         </div>
       </main>
     </div>
